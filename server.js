827ee8d1d6685b830271573efe237aa705b2544e
jberthiaume1138 <joenyc23@gmail.com>
2016-11-09 22:34:10 -0500

    server.js basics, add some files

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 var express = require('express');
 var socket_io = require('socket.io');
+var http = require('http');
+var util = require('util');
+
+var app = express();
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+app.use('*', function(req, res) {
+    res.send('Error 404 - page not found');
+    res.status(404);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+    util.log('.....Listening on port: ' + port + ' .....');
+});
